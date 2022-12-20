9de9d67b3b4a55ab4c45b5aab12a2f3bc1833143
weineel <lijufeng@patsnap.com>
2022-12-20 21:30:47 +0800

    feat: css keyword, all

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/css-keyword">css-keyword</RouterLink>
+        <!-- <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
   </header>
---
+++ src/views/CssKeyword.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="css-keyword">
+    <div class="word"> 这是话 </div>
+    <div class="word1"> 这是第一句话 </div>
+    <div class="word2"> 这是第二句话 </div>
+  </div>
+</template>
+
+<style scoped>
+.css-keyword {
+  color: red;
+  height: 200px;
+}
+.word1 {
+  color: blue;
+  /* display 属性的 initial 值是 inline */
+  all: initial;
+}
+
+.word2 {
+  all: initial;
+  color: blue;
+}
+</style>
